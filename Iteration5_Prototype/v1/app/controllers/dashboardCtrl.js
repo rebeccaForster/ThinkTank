13dfba18db675514068f2e22627bce9932063518
rebeccaForster <rebecca_forster@web.de>
2016-08-08 20:06:20 +0200

    hinzufügen der rechten spalte ovn der Ideenübersicht.
    Todo: togglen der react on idea icons und input dialoges

--- Iteration5_Prototype/v1/app/controllers/dashboardCtrl.js
+++ Iteration5_Prototype/v1/app/controllers/dashboardCtrl.js
@@ -1,110 +1,439 @@
 'use strict';
 
 app.controller('DashboardCtrl', function ($scope, $location, $mdDialog, $mdMedia) {
-
+    $scope.users = [
+        {
+            id: 0,
+            name: "Marius Mülle0",
+            profileImg: "app/img/user.jpg"
+        }, {
+            id: 1,
+            name: "Marius Mülle1",
+            profileImg: "app/img/user.jpg"
+        }, {
+            id: 2,
+            name: "Marius Mülle2",
+            profileImg: "app/img/user.jpg"
+        }, {
+            id: 3,
+            name: "Marius Mülle3",
+            profileImg: "app/img/user.jpg"
+        }, {
+            id: 4,
+            name: "Marius Mülle4",
+            profileImg: "app/img/user.jpg"
+        }, {
+            id: 5,
+            name: "Marius Mülle5",
+            profileImg: "app/img/user.jpg"
+        }, {
+            id: 6,
+            name: "Marius Mülle6",
+            profileImg: "app/img/user.jpg"
+        }, {
+            id: 7,
+            name: "Marius Mülle7",
+            profileImg: "app/img/user.jpg"
+        }
+    ]
     $scope.ideas = [
         {
             id: 0,
-            author: "Hans Petet 0",
+            author: 6,
             title: "Automotiva driving asdfkdasfj Automotiva driving asdfkdasfj Automotiva driving asdfkdasfj",
             tags: ["tag11", "tag21", "g11", "tag112", "tag212", "g112"],
-            conributor: ["cont1", "cont2"],
+            contributors: [1, 3, 4, 6, 7, 2],
             description: "blalfsldöfjsdafljdsakfljsdalf daksfjldfkjas dskfj sdafkljdöfjsdafljdsakfljsdalf daksfjldfkjas dskfdöfjsdafljdsakfljsdalf daksfjldfkjas dskf",
-            img: "http://placehold.it/100x50"
-        },
-        {
+            img: "http://placehold.it/100x50",
+            scribble: 'http://placehold.it/150x100',
+            milestones: [
+                {
+                    name: "find location",
+                    status: true
+                }, {
+                    name: "get founding",
+                    status: true
+                }, {
+                    name: "define project goal",
+                    status: false
+                }, {
+                    name: "find contributors",
+                    status: false
+                }, {
+                    name: "find business partners",
+                    status: true
+                }
+            ],
+            percentage: 40,
+            dayLeft: "6",
+            messages: [
+
+                {
+                    author: 2,
+                    text: "asdfl ldajf dsakfjldomotiva driving asdfkdasfj Automotiva driving asdfkdasfj Automotiva dr e alkjdklfjasd ekl re akdsfjl",
+                    likeIdeaStatus: true,
+                    newInputStatus: true,
+                    troubleStatus: true,
+                    other: false
+            }, {
+                    author: 7,
+                    text: "asdfl ldajf dsakfjldf omotiva driving asdfkdasfj Automotiva driving asdfkdasfj Automotiva dromotiva driving asdfkdasfj Automotiva driving asdfkdasfj Automotiva dre alkjdklfjasd ekl re akdsfjl",
+                    likeIdeaStatus: true,
+                    newInputStatus: false,
+                    troubleStatus: true,
+                    other: false
+            },
+                {
+                    author: 1,
+                    text: "asdfl ldajf dsakfjldf omotiva driving asdfkdasfj Automotiva driving asdfkdasfj Automotiva dromotiva driving asdfkdasfj Automotiva driving asdfkdasfj Automotiva dromotiva driving asdfkdasfj Automotiva driving asdfkdasfj Automotiva dre alkjdklfjasd ekl re akdsfjl",
+                    likeIdeaStatus: false,
+                    newInputStatus: false,
+                    troubleStatus: true,
+                    other: false
+            }
+
+            ]
+    }, {
             id: 1,
-            author: "Hans Petet 1",
+            author: 2,
             title: "Automotiva driving asdfkdasfj",
             tags: ["tag11", "tag21", "g11", "tag112", "tag212", "g112"],
-            conributor: ["cont1", "cont2"],
+            contributors: [1, 3, 6],
             description: "blalfsldöfjsdafljdsakfljsdalf daksfjldfkjas dskfj sdafkljdöfjsdafljdsakfljsdalf daksfjldfkjas dskfdöfjsdafljdsakfljsdalf daksfjldfkjas dskf",
-            img: "http://placehold.it/100x50"
-        }, {
+            img: "http://placehold.it/100x50",
+            scribble: 'http://placehold.it/150x100',
+            milestones: [
+                {
+                    name: "find location",
+                    status: true
+                }, {
+                    name: "get founding",
+                    status: true
+                }, {
+                    name: "define project goal",
+                    status: false
+                }, {
+                    name: "find contributors",
+                    status: false
+                }, {
+                    name: "find business partners",
+                    status: true
+                }
+            ],
+            percentage: 50,
+            dayLeft: "7",
+            messages: [
+
+                {
+                    author: 2,
+                    text: "asdfl ldajf dsakfjldf e alkjdklfjasd ekl re akdsfjl",
+                    likeIdeaStatus: true,
+                    newInputStatus: true,
+                    troubleStatus: true,
+                    other: false
+            }, {
+                    author: 7,
+                    text: "asdfl ldajf dsakfjldf e alkjdklfjasd ekl re akdsfjl",
+                    likeIdeaStatus: true,
+                    newInputStatus: true,
+                    troubleStatus: true,
+                    other: false
+            },
+                {
+                    author: 1,
+                    text: "asdfl ldajf dsakfjldf e alkjdklfjasd ekl re akdsfjl",
+                    likeIdeaStatus: true,
+                    newInputStatus: true,
+                    troubleStatus: true,
+                    other: false
+            }
+
+            ]
+    }, {
             id: 2,
-            author: "Hans Petet 2",
+            author: 0,
             title: "Automotiva driving asdfkdasfj",
             tags: ["tag12", "tag22"],
-            conributor: ["cont1", "cont2"],
+            contributors: [1, 4, 6],
             description: "blalfsldöfjsdafljdsakfljsdalf daksfjldfkjas dskfdöfjsdafljdsakfljsdalf daksfjldfkjas dskfj sdafklj",
-            img: ""
-        }, {
-            id: 3,
-            author: "Hans Petet 3",
-            title: "Automotiva driving asdfkdasfj",
-            tags: ["tag1", "tag2"],
-            conributor: ["cont1", "cont2"],
-            description: "blalfsldöfjsdafljdsakfljsdalf daksfjldfkjas dskfj sdafklj",
-            img: "http://placehold.it/100x50"
+            img: "",
+            scribble: '',
+            milestones: [
+                {
+                    name: "find location",
+                    status: true
+                }, {
+                    name: "get founding",
+                    status: true
+                }, {
+                    name: "define project goal",
+                    status: false
+                }, {
+                    name: "find contributors",
+                    status: false
+                }, {
+                    name: "find business partners",
+                    status: true
+                }
+            ],
+            percentage: 60,
+            dayLeft: "8",
+            messages: [
 
-        }, {
+                {
+                    author: 2,
+                    text: "asdfl ldajf dsakfjldf e alkjdklfjasd ekl re akdsfjl",
+                    likeIdeaStatus: true,
+                    newInputStatus: true,
+                    troubleStatus: true,
+                    other: false
+            }, {
+                    author: 7,
+                    text: "asdfl ldajf dsakfjldf e alkjdklfjasd ekl re akdsfjl",
+                    likeIdeaStatus: true,
+                    newInputStatus: true,
+                    troubleStatus: true,
+                    other: false
+            },
+                {
+                    author: 1,
+                    text: "asdfl ldajf dsakfjldf e alkjdklfjasd ekl re akdsfjl",
+                    likeIdeaStatus: true,
+                    newInputStatus: true,
+                    troubleStatus: true,
+                    other: false
+            }
+
+            ]
+    }, {
+            id: 3,
+            author: 3,
+            title: "Automotiva driving asdfkdasfj",
+            tags: ["tag1", "tag2"],
+            contributors: ["cont1", "cont2"],
+            description: "blalfsldöfjsdafljdsakfljsdalf daksfjldfkjas dskfj sdafklj",
+            img: "http://placehold.it/100x50",
+            scribble: 'http://placehold.it/150x100',
+            milestones: [
+                {
+                    name: "find location",
+                    status: true
+                }, {
+                    name: "get founding",
+                    status: true
+                }, {
+                    name: "define project goal",
+                    status: false
+                }, {
+                    name: "find contributors",
+                    status: false
+                }, {
+                    name: "find business partners",
+                    status: true
+                }
+            ],
+            percentage: 70,
+            dayLeft: "9",
+            messages: [
+
+                {
+                    author: 2,
+                    text: "asdfl ldajf dsakfjldf e alkjdklfjasd ekl re akdsfjl",
+                    likeIdeaStatus: true,
+                    newInputStatus: true,
+                    troubleStatus: true,
+                    other: false
+            }, {
+                    author: 7,
+                    text: "asdfl ldajf dsakfjldf e alkjdklfjasd ekl re akdsfjl",
+                    likeIdeaStatus: true,
+                    newInputStatus: true,
+                    troubleStatus: true,
+                    other: false
+            },
+                {
+                    author: 1,
+                    text: "asdfl ldajf dsakfjldf e alkjdklfjasd ekl re akdsfjl",
+                    likeIdeaStatus: true,
+                    newInputStatus: true,
+                    troubleStatus: true,
+                    other: false
+            }
+
+            ]
+
+    }, {
             id: 4,
-            author: "Hans Petet 4",
+            author: 6,
             title: "Automotiva driving asdfkdasfj",
             tags: ["tag1", "tag2"],
-            conributor: ["cont1", "cont2"],
+            contributors: [4, 3, 6],
             description: "blalfsldöfjsdafljdsakfljsdalf daksfjldfkjasdöfjsdafljdsakfljsdalf daksfjldfkjasj",
-            img: "http://placehold.it/100x50"
-        }, {
+            img: "http://placehold.it/100x50",
+            scribble: 'http://placehold.it/150x100',
+            milestones: [
+                {
+                    name: "find location",
+                    status: true
+                }, {
+                    name: "get founding",
+                    status: true
+                }, {
+                    name: "define project goal",
+                    status: false
+                }, {
+                    name: "find contributors",
+                    status: false
+                }, {
+                    name: "find business partners",
+                    status: true
+                }
+            ],
+            percentage: 80,
+            dayLeft: "10",
+            messages: [
+
+                {
+                    author: 2,
+                    text: "asdfl ldajf dsakfjldf e alkjdklfjasd ekl re akdsfjl",
+                    likeIdeaStatus: true,
+                    newInputStatus: true,
+                    troubleStatus: true,
+                    other: false
+            }, {
+                    author: 7,
+                    text: "asdfl ldajf dsakfjldf e alkjdklfjasd ekl re akdsfjl",
+                    likeIdeaStatus: true,
+                    newInputStatus: true,
+                    troubleStatus: true,
+                    other: false
+            },
+                {
+                    author: 1,
+                    text: "asdfl ldajf dsakfjldf e alkjdklfjasd ekl re akdsfjl",
+                    likeIdeaStatus: true,
+                    newInputStatus: true,
+                    troubleStatus: true,
+                    other: false
+            }
+
+            ]
+    }, {
             id: 5,
-            author: "Hans Petet 5",
+            author: 7,
             title: "Automotiva driving asdfkdasfj",
             tags: ["tag1", "tag2"],
-            conributor: ["cont1", "cont2"],
+            contributors: [1, 2, 6],
             description: "blalfsldöfjsdafljdsakfljsdalf daksfjldfkjas dskfj sdafklj",
-            img: "http://placehold.it/100x50"
-        }, {
+            img: "http://placehold.it/100x50",
+            scribble: 'http://placehold.it/150x100',
+            milestones: [
+                {
+                    name: "find location",
+                    status: true
+                }, {
+                    name: "get founding",
+                    status: true
+                }, {
+                    name: "define project goal",
+                    status: false
+                }, {
+                    name: "find contributors",
+                    status: false
+                }, {
+                    name: "find business partners",
+                    status: true
+                }
+            ],
+            percentage: 90,
+            dayLeft: "11",
+            messages: [
+
+                {
+                    author: 2,
+                    text: "asdfl ldajf dsakfjldf e alkjdklfjasd ekl re akdsfjl",
+                    likeIdeaStatus: true,
+                    newInputStatus: true,
+                    troubleStatus: true,
+                    other: false
+            }, {
+                    author: 7,
+                    text: "asdfl ldajf dsakfjldf e alkjdklfjasd ekl re akdsfjl",
+                    likeIdeaStatus: true,
+                    newInputStatus: true,
+                    troubleStatus: true,
+                    other: false
+            },
+                {
+                    author: 1,
+                    text: "asdfl ldajf dsakfjldf e alkjdklfjasd ekl re akdsfjl",
+                    likeIdeaStatus: true,
+                    newInputStatus: true,
+                    troubleStatus: true,
+                    other: false
+            }
+
+            ]
+    }, {
             id: 6,
-            author: "Hans Petet 6",
+            author: "6",
             title: "Automotiva driving asdfkdasfj",
             tags: ["tag1", "tag2"],
-            conributor: ["cont1", "cont2"],
+            contributors: [1, 3, 7],
             description: "blalfsldöfjsdafljdsakfljsdalf daksfjldfkjadöfjsdafljdsakfljsdalf daksfjldfkjas dskfdöfjsdafljdsakfljsdalf daksfjldfkjas dskfs dskfj sdafklj",
-            img: "http://placehold.it/100x50"
-        }, {
-            id: 7,
-            author: "Hans Petet 7",
-            title: "Automotiva driving asdfkdasfj",
-            tags: ["tag1", "tag2"],
-            conributor: ["cont1", "cont2"],
-            description: "blalfsldöfjsdafljdsakfljsdalf daksfjldfkjdöfjsdafljdsakfljsdalf daksfjldfkjas dskfdöfjsdafljdsakfljsdalf daksfjldfkjas dskfdöfjsdafljdsakfljsdalf daksfjldfkjas dskfdöfjsdafljdsakfljsdalf daksfjldfkjas dskfdöfjsdafljdsakfljsdalf daksfjldfkjas dskfas dskfj sdafklj",
-            img: ""
-      }, {
-            id: 8,
-            author: "Hans Petet 4",
-            title: "Automotiva driving asdfkdasfj",
-            tags: ["tag1", "tag2"],
-            conributor: ["cont1", "cont2"],
-            description: "blalfsldöfjsdafljdsakfljsdalf daksfjldfkjasdöfjsdafljdsakfljsdalf daksfjldfkjasj",
-            img: "http://placehold.it/100x50"
-        }, {
-            id: 9,
-            author: "Hans Petet 5",
-            title: "Automotiva driving asdfkdasfj",
-            tags: ["tag1", "tag2"],
-            conributor: ["cont1", "cont2"],
-            description: "blalfsldöfjsdafljdsakfljsdalf daksfjldfkjas dskfj sdafklj",
-            img: "http://placehold.it/100x50"
-        }, {
-            id: 10,
-            author: "Hans Petet 6",
-            title: "Automotiva driving asdfkdasfj",
-            tags: ["tag1", "tag2"],
-            conributor: ["cont1", "cont2"],
-            description: "blalfsldöfjsdafljdsakfljsdalf daksfjldfkjadöfjsdafljdsakfljsdalf daksfjldfkjas dskfdöfjsdafljdsakfljsdalf daksfjldfkjas dskfs dskfj sdafklj",
-            img: "http://placehold.it/100x50"
-        }, {
-            id: 11,
-            author: "Hans Petet 7",
-            title: "Automotiva driving asdfkdasfj",
-            tags: ["tag1", "tag2"],
-            conributor: ["cont1", "cont2"],
-            description: "blalfsldöfjsdafljdsakfljsdalf daksfjldfkjdöfjsdafljdsakfljsdalf daksfjldfkjas dskfdöfjsdafljdsakfljsdalf daksfjldfkjas dskfdöfjsdafljdsakfljsdalf daksfjldfkjas dskfdöfjsdafljdsakfljsdalf daksfjldfkjas dskfdöfjsdafljdsakfljsdalf daksfjldfkjas dskfas dskfj sdafklj",
-            img: ""
-      }
+            img: "http://placehold.it/100x50",
+            scribble: 'http://placehold.it/150x100',
+            milestones: [
+                {
+                    name: "find location",
+                    status: true
+                }, {
+                    name: "get founding",
+                    status: true
+                }, {
+                    name: "define project goal",
+                    status: false
+                }, {
+                    name: "find contributors",
+                    status: false
+                }, {
+                    name: "find business partners",
+                    status: true
+                }
+            ],
+            percentage: 100,
+            dayLeft: "123",
+            messages: [
+
+                {
+                    author: 2,
+                    text: "asdfl ldajf dsakfjldf e alkjdklfjasd ekl re akdsfjl",
+                    likeIdeaStatus: true,
+                    newInputStatus: true,
+                    troubleStatus: true,
+                    other: false
+            }, {
+                    author: 7,
+                    text: "asdfl ldajf dsakfjldf e alkjdklfjasd ekl re akdsfjl",
+                    likeIdeaStatus: true,
+                    newInputStatus: true,
+                    troubleStatus: true,
+                    other: false
+            },
+                {
+                    author: 1,
+                    text: "asdfl ldajf dsakfjldf e alkjdklfjasd ekl re akdsfjl",
+                    likeIdeaStatus: true,
+                    newInputStatus: true,
+                    troubleStatus: true,
+                    other: false
+            }
+
+            ]
+    }
     ];
 
-    $scope.maxColumn = 3
+    $scope.maxColumn = 3;
     $scope.commentIdea = function (index, ev) {
         $mdDialog.show(
             $mdDialog.alert()
@@ -216,7 +545,9 @@ app.controller('gridListDemoCtrl', function ($scope) {
 
 
 function DialogController($scope, $mdDialog, dashboardScope) {
-    $scope.selectedIdea = dashboardScope.ideas[dashboardScope.selectedIndex]
+    $scope.selectedIdea = dashboardScope.ideas[dashboardScope.selectedIndex];
+    $scope.users = dashboardScope.users;
+
     $scope.hide = function () {
         $mdDialog.hide();
     };
