471ee4c5ff8b9ea880762b24a159ebdbb854028f
rebeccaForster <rebecca_forster@web.de>
2016-08-15 17:25:22 +0200

    upload changes toolbar, hashtags

--- Iteration5_Prototype/v1.2/public/app/controllers/dashboardProfileCtrl.js
+++ Iteration5_Prototype/v1.2/public/app/controllers/dashboardProfileCtrl.js
@@ -121,12 +121,221 @@ angular
 
 
     };
+    
+     $scope.searchHashtag = function (ev) {
+        $mdDialog.show({
+                controller: HashtagPopupController,
+                templateUrl: 'app/views/hashtag-popup.html',
+                targetEvent: ev,
+                scope: $scope, // use parent scope in template
+                preserveScope: true,
+                clickOutsideToClose: true,
+                fullscreen: true,
+                locals: {}
+            })
+            .then(function () {}, function () {
+                $scope.updateDashboard();
+            });
+    }
+
+    $scope.getStatusHashtag = function (id) {
+
+        for (var i in $scope.selectedHashtags) {
+            if ($scope.selectedHashtags[i] == id) {
+                return true;
+            }
+        }
+        return false;
+    }
+    $scope.selectedHashtags = [];
+
+    $scope.sorting = [
+        {
+            "id": 0,
+            "title": "Latest Ideas"
+        }, {
+            "id": 1,
+            "title": "Most popular"
+        }, {
+            "id": 2,
+            "title": "Friedhof"
+        }, {
+            "id": 3,
+            "title": "Himmel"
+        }];
+
+    $scope.getHashtagStyle = function (priority) {
+
+        var size = "0px";
+        switch (priority) {
+        case 0:
+            size = "12px"
+            break;
+        case 1:
+            size = "14px"
+            break;
+        case 2:
+            size = "16px"
+            break;
+        case 3:
+            size = "18px"
+            break;
+        case 4:
+            size = "20px"
+            break;
+
+        default:
+            size = "12px"
+        }
+        return {
+            "font-size": size
+        }
+    }
+
+    //init
+    $scope.sortingType = $scope.sorting[0].title;
+    $scope.hashtags = [
+        {
+            id: 0,
+            name: 'aReis',
+            priority: 0
+        }, {
+            id: 1,
+            name: 'wPepperoni',
+            priority: 2
+        },
+        {
+            id: 2,
+            name: 'eSausage',
+            priority: 4
+        },
+        {
+            id: 3,
+            name: 'Black Olives',
+            priority: 1
+        },
+        {
+            id: 4,
+            name: 'rGreen Peppers',
+            priority: 3
+        },
+        {
+            id: 5,
+            name: 'gSausage',
+            priority: 4
+        },
+        {
+            id: 6,
+            name: 'kBlack Olives',
+            priority: 1
+        },
+        {
+            id: 7,
+            name: 'uGreen Peppers',
+            priority: 3
+        },
+        {
+            id: 8,
+            name: 'dBlack Olives',
+            priority: 1
+        },
+        {
+            id: 9,
+            name: 'hGreen Peppers',
+            priority: 3
+        }, {
+            id: 10,
+            name: 'kReis',
+            priority: 0
+        }, {
+            id: 11,
+            name: 'mvPepperoni',
+            priority: 2
+        },
+        {
+            id: 12,
+            name: 'Sausage',
+            priority: 4
+        },
+        {
+            id: 13,
+            name: 'vBlack Olives',
+            priority: 1
+        },
+        {
+            id: 14,
+            name: 'dGreen Peppers',
+            priority: 3
+        }, {
+            id: 15,
+            name: 'rPepperoni',
+            priority: 2
+        },
+        {
+            id: 16,
+            name: 'daBlack Olives',
+            priority: 1
+        },
+        {
+            id: 17,
+            name: 'fGreen Peppers',
+            priority: 3
+        },
+        {
+            id: 18,
+            name: 'nBlack Olives',
+            priority: 1
+        },
+        {
+            id: 19,
+            name: 'gGreen Peppers',
+            priority: 3
+        }
+  ];
+    $scope.hashtagSelected = function (id) {
+        for (var i in $scope.selectedHashtags) {
+            if ($scope.selectedHashtags[i] == id) {
+                return true;
+            }
+        }
+        return false;
+    }
+    $scope.setHashtags = function (id, status) {
+        if (!status) {
+            $scope.selectedHashtags.push(id);
+        } else {
+            $scope.selectedHashtags.splice($scope.selectedHashtags.indexOf(id), 1);
+
+        }
+    }
+    $scope.changeSortingType = function(index){
+        $scope.sortingType= index;
+        $scope.updateDashboard();
+    
+    }
+    $scope.updateDashboard = function () {
+        // ToDo: es wurden neue Tags hinzugefügt bzw. entfernt und hier müsstest du mithilfe der Tags & des auswählten Sorting die Liste erneuern
+        // $scope.sortingType gibt den Namen der Sortierung zurück
+        // $scope.selectedHashtags  gibt alle Tags IDs an, nach denen man suchen soll
+    }
+
 
     
 
 });
 
 
+
+function HashtagPopupController($scope, $mdDialog) {
+
+    $scope.hide = function () {
+        $mdDialog.hide();
+    };
+    $scope.cancel = function () {
+        $mdDialog.cancel();
+    };
+
+}
 
 function IdeaPopupController($scope, $mdDialog, ideaIndex) {
     $scope.selectedIdea = $scope.ideas[ideaIndex];
