7dce2683f64fd46192019c7e5d0506f12eb135f8
thinkTankTUM <thinkTankTUM@gmail.com>
2016-08-20 17:32:16 +0200

    hinzufügen von whiteboard icon und der save fuktion im whiteboard; einer batch datei für windows die server und db startet
    das whiteboard ist noch nicth fertig

--- Iteration5_Prototype/v1.2/public/app/controllers/whiteboardCtrl.js
+++ Iteration5_Prototype/v1.2/public/app/controllers/whiteboardCtrl.js
@@ -1,4 +1,79 @@
 'use strict';
-app.controller('WhiteboardCtrl', function($scope) {
-        
-    });
+app.controller('WhiteboardCtrl', function ($scope, authentication, $mdDialog) {
+    $scope.saveScribble = function (ev) {
+        $mdDialog.show({
+                controller: SaveDialogController,
+                templateUrl: 'app/views/whiteboard-save-popup.html',
+                targetEvent: ev,
+                scope: $scope, // use parent scope in template
+                preserveScope: true,
+                clickOutsideToClose: true,
+                fullscreen: true,
+                locals: {
+                    authentication: authentication
+
+                }
+
+            })
+            .then(function () {
+                return true;
+            }, function () {
+                return true;
+            });
+
+
+
+
+    };
+    $scope.hashtags = [];
+    $scope.contributors = [];
+    $scope.desciption = "";
+    $scope.milestons = [];
+    $scope.title = "";
+});
+
+
+function SaveDialogController($scope, $mdDialog, authentication) {
+    $scope.credentials = {
+        email: "",
+        password: ""
+    };
+    $scope.placeholderTitle = "aktuells daum udn Uhrzeit";
+    $scope.title = "";
+    $scope.hide = function () {
+        $mdDialog.hide();
+    };
+    $scope.cancel = function () {
+        $mdDialog.cancel();
+    };
+    $scope.save = function () {
+        if($scope.isLoggedIn){
+        authentication
+            .login($scope.credentials)
+            .then(function () {
+                $scope.cancel();
+                $scope.setSignInStatus();
+                /*Todo    erstellen einer Idee mit $scope.hashtags =[];
+    $scope.contributors 
+    $scope.desciption 
+    $scope.milestons 
+    $scope.title = 
+    dem aktuellen datum und Uhrzeit 
+    scribble als vektordatei
+ */
+            });
+        }else{
+                            $scope.cancel();
+
+            /*Todo    erstellen einer Idee mit $scope.hashtags =[];
+    $scope.contributors 
+    $scope.desciption 
+    $scope.milestons 
+    $scope.title = 
+    dem aktuellen datum und Uhrzeit 
+    scribble als vektordatei
+ */
+        }
+
+    };
+}
