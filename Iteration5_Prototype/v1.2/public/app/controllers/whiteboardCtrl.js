25c1cb06ff2e8892a7a8949dcf901dbffc76aae8
thinkTankTUM <thinkTankTUM@gmail.com>
2016-08-25 20:45:27 +0200

    update canvas layout design whiteboard

--- Iteration5_Prototype/v1.2/public/app/controllers/whiteboardCtrl.js
+++ Iteration5_Prototype/v1.2/public/app/controllers/whiteboardCtrl.js
@@ -1,5 +1,5 @@
 'use strict';
-app.controller('WhiteboardCtrl', function ($scope, authentication, $mdDialog) {
+app.controller('WhiteboardCtrl', function ($scope, authentication, $mdDialog, $window) {
     $scope.saveScribble = function (ev) {
         $mdDialog.show({
                 controller: SaveDialogController,
@@ -30,60 +30,71 @@ app.controller('WhiteboardCtrl', function ($scope, authentication, $mdDialog) {
 
     //DrawingBoard
     // $localStorage.$reset();
-    
-      $scope.drawingModes = ['draw', 'eraser', 'fill'];
-      $scope.webStorage = 'session';
-      $scope.drawingMode = 'draw';
-      $scope.drawColor = '#222';
-      $scope.eraseColor = '#EEE';
-      $scope.lineWidth = 3;
-      $scope.backgroundColor = '#EEE';
-      $scope.canvasWidth = '800';
-      $scope.canvasHeight = '800';
-      
-      $scope.drawingboardRemote = {
-        'startDraw': function(event) {
-          console.log('start drawing', event);
-        },
-        'endDraw': function(event) {
-          console.log('end drawing', event);
-        },
-        'drawing': function(event) {
-          //console.log('drawing', event);
-        },
-        'startErase': function(event) {
-          console.log('start erase', event);
-        },
-        'endErase': function(event) {
-          console.log('end erase', event);
-        },
-        'erasing': function(event) {
-          //console.log('erasing', event);
-        },
-        'fill': function(event) {
-          console.log('filled', event);
+
+    $scope.drawingModes = ['draw', 'eraser', 'fill'];
+    $scope.webStorage = 'session';
+    $scope.drawingMode = 'draw';
+    $scope.drawColor = '#222';
+    $scope.eraseColor = '#EEE';
+    $scope.lineWidth = 3;
+    $scope.backgroundColor = '#EEE';
+
+    $scope.setCanvasStyle = function () {
+        var height = document.getElementById('whiteboard-main').offsetHeight -5;
+        console.log(height);
+        var width = document.getElementById('whiteboard-main').offsetWidth -90;
+         $scope.canvasWidth =  width;
+    $scope.canvasHeight = height;
+         $scope.clear();
+        return {
+            "height": height + 'px',
+            "width": width+ 'px'
         }
-      };
-      
-      $scope.getDataURL = function() {
+    }
+
+    $scope.drawingboardRemote = {
+        'startDraw': function (event) {
+            console.log('start drawing', event);
+        },
+        'endDraw': function (event) {
+            console.log('end drawing', event);
+        },
+        'drawing': function (event) {
+            //console.log('drawing', event);
+        },
+        'startErase': function (event) {
+            console.log('start erase', event);
+        },
+        'endErase': function (event) {
+            console.log('end erase', event);
+        },
+        'erasing': function (event) {
+            //console.log('erasing', event);
+        },
+        'fill': function (event) {
+            console.log('filled', event);
+        }
+    };
+
+    $scope.getDataURL = function () {
         console.log($scope.drawingboardRemote.toDataURL('image/png'));
-      };
-      
-      $scope.clear = function() {
+    };
+
+    $scope.clear = function () {
         console.log($scope.drawingboardRemote.clear());
-      };
-      
-      $scope.undo = function() {
+    };
+
+    $scope.undo = function () {
         console.log($scope.drawingboardRemote.undo());
-      };
-      
-      $scope.redo = function() {
+    };
+
+    $scope.redo = function () {
         console.log($scope.drawingboardRemote.redo());
-      };
-      
-      $scope.clearHistory = function() {
+    };
+
+    $scope.clearHistory = function () {
         console.log($scope.drawingboardRemote.clearStorage());
-      };
+    };
 
 });
 
@@ -102,13 +113,13 @@ function SaveDialogController($scope, $mdDialog, authentication) {
         $mdDialog.cancel();
     };
     $scope.save = function () {
-        if($scope.isLoggedIn){
-        authentication
-            .login($scope.credentials)
-            .then(function () {
-                $scope.cancel();
-                $scope.setSignInStatus();
-                /*Todo    erstellen einer Idee mit $scope.hashtags =[];
+        if ($scope.isLoggedIn) {
+            authentication
+                .login($scope.credentials)
+                .then(function () {
+                    $scope.cancel();
+                    $scope.setSignInStatus();
+                    /*Todo    erstellen einer Idee mit $scope.hashtags =[];
     $scope.contributors 
     $scope.desciption 
     $scope.milestons 
@@ -116,9 +127,9 @@ function SaveDialogController($scope, $mdDialog, authentication) {
     dem aktuellen datum und Uhrzeit 
     scribble als vektordatei
  */
-            });
-        }else{
-                            $scope.cancel();
+                });
+        } else {
+            $scope.cancel();
 
             /*Todo    erstellen einer Idee mit $scope.hashtags =[];
     $scope.contributors 
