e5211e8dcd27f0ba94ed8b5370e62ed99757bad9
Frederic Wollinger <fredericwollinger@gmail.com>
2016-09-07 11:06:45 +0200

    II

--- Iteration5_Prototype/v1.2/models/comment.model.js
+++ Iteration5_Prototype/v1.2/models/comment.model.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 var CommentSchema = new mongoose.Schema({
 	created: { type: Date, default: Date.now }, 
-	ikeIdeaStatus: Boolean,
+	likeIdeaStatus: Boolean,
   	newInputStatus: Boolean,
   	troubleStatus: Boolean,
   	otherreaction: Boolean,
